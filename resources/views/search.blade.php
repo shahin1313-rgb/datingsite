ec52c76ad3d2f18c4099cf63cffff0f23f42ea2d
shahin1917 <shahin1917@gmail.com>
2025-07-03 09:55:37 +0330

    pak kardan code ezafi dar search comment bod

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,95 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-
-                    <h1>Search Profiles</h1>
-                      <form action="{{ route('search') }}" method="GET">
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text" id="city">City:</span>
-                        </div>
-                        <input type="text" class="form-control" placeholder="city" name="city" id="city" value="{{ request('city') }}" aria-label="city" aria-describedby="basic-addon1">
-                      </div>
-                      <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text" id="age"> min age:</span>
-                        </div>
-                        <input   type="number" name="min_age" id="min_age" value="{{ request('min_age') }}" class="form-control" placeholder="age" aria-label="age" aria-describedby="basic-addon1">
-
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="age"> Max age:</span>
-                          </div>
-                          <input   type="number" name="max_age" id="max_age" value="{{ request('max_age') }}" class="form-control" placeholder="age" aria-label="age" aria-describedby="basic-addon1">
-
-
-
-                    </div>
-
-
-                      <div class="input-group-prepend ">
-                        <button class="btn btn-outline-secondary" type="submit">Search</button>
-                      </div>
-
-                    </form>
-
-
-                    <div class="card align-items-center" style="width: 18rem;">
-                        @if ($profiles->isEmpty())
-                        <p>No profiles found.</p>
-                         @else
-                        <ul>
-                            @foreach ($profiles as $profile)
-                                <li>
-                        @if ($profile->profile_picture)
-                            <img src="{{ $profile->profile_picture ? asset('storage/' . $profile->profile_picture) : asset('images/default-profile.jpg') }}"
-                                class="card-img-top" alt="Profile Picture">
-                        @else
-                            <p>No profile picture available.</p>
-                        @endif
-
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $profile->name }}</h5>
-                            <h5 class="card-title">{{ $profile->city }}</h5>
-
-                            <h5 class="card-title">{{ $profile->age }}</h5>
-                            <h5 class="card-title">{{ $profile->birth_year }}</h5>
-
-                            <p class="card-text">{{ $profile->email }}</p>
-                            <p class="card-text">{{ $profile->bio }}</p>
-
-                            <a href="#" class="btn btn-primary">show</a>
-
-
-                        </div>
-                           </li>
-                            @endforeach
-                        </ul>
-
-                        <!-- Pagination Links -->
-                        {{ $profiles->appends(request()->query())->links() }}
-                    </div>
-
-
-
-
-
-
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-@endsection
-
- --}}
-
 @extends('layouts.app')
 
 @section('content')
@@ -136,9 +44,8 @@
                                             {{-- <img src="{{ $profile->profile_picture ? asset('storage/' . $profile->profile_picture) : asset('images/default-profile.jpg') }}"
 
                                             class="card-img-top" alt="Profile Picture"> --}}
-                                            <img class="rounded-circle shadow-4-strong card-img-top" alt="avatar2" src="{{asset('storage/' . $profile->profile_picture)}}" />
-
-
+                                            <img class="rounded-circle shadow-4-strong card-img-top" alt="avatar2"
+                                                src="{{ asset('storage/' . $profile->profile_picture) }}" />
                                         @else
                                             <p>No profile picture available.</p>
                                         @endif
@@ -152,11 +59,12 @@
                                             <p class="card-text">{{ $profile->bio }}</p>
 
                                             <div class="d-flex gap-2">
-                                            <a href="{{ route('messages.show', $profile) }}" class="btn btn-primary ">
-                                                Send Message
-                                            </a>
+                                                <a href="{{ route('messages.show', $profile) }}" class="btn btn-primary ">
+                                                    Send Message
+                                                </a>
 
-                                            <a href="{{ route('profile.show', $profile->id) }}" class="btn btn-primary">View Profile</a>
+                                                <a href="{{ route('profile.show', $profile->id) }}"
+                                                    class="btn btn-primary">View Profile</a>
 
                                             </div>
                                         </div>
--- routes/web.php
+++ routes/web.php
@@ -24,39 +24,38 @@ Route::middleware('auth')->group(function () {
 
 
 
-// Password Reset Routes
-Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+    // Password Reset Routes ff
+    Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 
-// Profile Update
-Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    // Profile Update
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
 
-Route::get('/profile/{name}', [HomeController::class, 'showname'])->name('profile');
+    Route::get('/profile/{name}', [HomeController::class, 'showname'])->name('profile');
 
-Route::get('/search', [ProfileController::class, 'search'])->name('search');
+    Route::get('/search', [ProfileController::class, 'search'])->name('search');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/profile/{id}', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/{id}', [ProfileController::class, 'show'])->name('profile.show');
 
-// Inbox page
-Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+    // Inbox page
+    Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
 
-// Conversation between two users
-Route::get('/messages/{user}', [MessageController::class, 'show'])->name('messages.show');
+    // Conversation between two users
+    Route::get('/messages/{user}', [MessageController::class, 'show'])->name('messages.show');
 
-// Send a message
-Route::post('/messages/{user}', [MessageController::class, 'store'])->name('messages.store');
+    // Send a message
+    Route::post('/messages/{user}', [MessageController::class, 'store'])->name('messages.store');
 
-// Dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 ///
 
@@ -71,7 +70,7 @@ Route::patch('/admin/make-admin/{id}', [AdminController::class, 'makeAdmin'])->n
 
 Route::patch('/admin/toggle-ban/{id}', [AdminController::class, 'toggleBan'])->name('admin.toggleBan');
 
-///// Report
+///// Report h
 Route::get('/admin/reports', [ReportController::class, 'index'])->name('admin.reports');
 Route::delete('/admin/reports/{id}', [ReportController::class, 'destroy'])->name('admin.reports.destroy');
 ///
@@ -82,19 +81,12 @@ Route::get('/admin/reports', [AdminController::class, 'showReports'])->name('adm
 Route::middleware('guest')->group(function () {
 
 
-// Authentication Routes
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+    // Authentication Routes
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
 
 
-// Registration Routes
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
-
-
+    // Registration Routes
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
 });
-
-
-
-
-
