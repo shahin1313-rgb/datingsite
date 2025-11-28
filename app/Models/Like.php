c488d206c59b0d42dc7b9a94d5e263beded01b8e
shahin1917 <shahin1917@gmail.com>
2025-11-28 22:26:54 +0330

    like show presormence in dashbord done
    نمایش کلید  لایک در داشبورد

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -3,49 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Like extends Model
 {
     use HasFactory;
 
-    // فیلدهایی که اجازه mass assignment دارند
     protected $fillable = [
-        'user_id',        // کاربری که لایک کرده
-        'liked_user_id',  // کاربری که لایک شده
+        'user_id',       
+        'liked_user_id', 
     ];
 
-    /**
-     * کاربری که لایک کرده
-     */
-    public function user()
+    // کاربری که لایک کرده
+    public function liker()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    /**
-     * کاربری که لایک شده
-     */
+    // کاربری که لایک شده
     public function likedUser()
     {
         return $this->belongsTo(User::class, 'liked_user_id');
-    }
-
-
-    /**
-     * Get the users that this user has liked.
-     */
-    public function likes()
-    {
-        return $this->hasMany(Like::class, 'user_id');
-    }
-
-    /**
-     * Get the users who have liked this user.
-     */
-    public function likedBy()
-    {
-        return $this->hasMany(Like::class, 'liked_user_id');
     }
 }
